7c102e8e14cf050396aa7f2e25271efbeafedbbb
Patrick <doyle1175@gmail.com>
2023-05-01 18:30:00 -0400

    Updated Home Page style and formatting

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,19 @@
 import React from 'react';
+import '../style/home.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="home">
+      <div className="home__text-container">
+        <h1 className="home__heading">Welcome to my portfolio</h1>
+        <p className="home__paragraph">
+          Hi, I'm Patrick Doyle, a front end developer with a passion for creating
+          beautiful, responsive web experiences. Take a look at my work below,
+          and feel free to get in touch if you'd like to collaborate on a
+          project.
+        </p>
+        <button className="home__cta-button">View My Work</button>
+      </div>
     </div>
   );
 }
